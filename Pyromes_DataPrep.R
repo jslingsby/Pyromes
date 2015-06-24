f27ccafd4000de09b930e16373fa05d2bb17e133
jslingsby <jslingsby@gmail.com>
2015-06-24 13:45:54 +0200

    Nikki and Jasper meeting 24June2015

--- Pyromes_DataPrep.R
+++ Pyromes_DataPrep.R
@@ -21,9 +21,7 @@ if (Sys.getenv("USER")=='jasper') {setwd("/Users/jasper/Dropbox/Shared/Pyromes")
 if (Sys.getenv("USERNAME")=='nikki') {setwd("C:/Users/nikki/Dropbox/Pyromes");.libPaths("C:/Users/nikki/R_libraries")}
 #if (Sys.getenv("USERNAME")=='WINDOZE PC') {datwd=""; giswd=""}
 
-# installing/loading the package:
-
-# step by step functions:
+# installing/loading the packages:
 library(maptools)
 library(raster) 
 library(rgdal)
@@ -35,7 +33,6 @@ require(fields)
 library(shapefiles)
 library(GISTools)
 require(PBSmapping)
-
 
 ##############################################################################
 ###2) Read in spatial data using RGdal command which includes the projection files
@@ -52,24 +49,20 @@ WNP<- readOGR (dsn=".","GRNP Firerecords_TKraaij")
 ## FIRE INTENSITY VALUES
 FI<- readOGR(dsn=".","Fire intensity")
 
-
-##check and set the projection
+###check and set the projection
 proj4string(CP)
 proj4string(CN)
 proj4string(BNP)
 proj4string(WNP)
 proj4string(FI)
 
-
-
-####Assign proj to be all the same
-
-
+###Assign proj to be all the same
 CP=spTransform(CP, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 BNP=spTransform(BNP, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 CN=spTransform(CN, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 WNP=spTransform(WNP, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 FI=spTransform(FI, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
 ##############################################################################
 ###3) Make matching fields and combine
 ##############################################################################
@@ -98,63 +91,98 @@ CN$SOURCE=rep("CN",length(CN))
 CN=CN[,c(1,4,3)]
 WNP=WNP[,c(10,3,9)]
 BNP=BNP[,c(9,3,4)]
-CP=CP[,c(8,1,10)]
+CP=CP[,c(9,1,11)]
 
 #Relable feature IDs so we can merge
-BNP <- spChFIDs(BNP, as.character(BNP$ID))
-WNP <- spChFIDs(WNP, as.character(WNP$ID))
-CN <- spChFIDs(CN, as.character(CN$ID))
-CP <- spChFIDs(CP, as.character(CP$ID))
+BNP = spChFIDs(BNP, as.character(BNP$ID))
+WNP = spChFIDs(WNP, as.character(WNP$ID))
+CN = spChFIDs(CN, as.character(CN$ID))
+CP = spChFIDs(CP, as.character(CP$ID))
 
 #Merge datasets
 dat=spRbind(CN, WNP)
 dat=spRbind(dat, BNP)
 dat=spRbind(dat, CP)
-#dat <- spChFIDs(dat, as.character(1:length(dat)))
 dat$YEAR_MONTH=paste(dat$YEAR, dat$MONTH, sep="_")
-#dat$DATE=
+dat$AREA_KM <- sapply(sapply(slot(dat, "polygons"), function(x) sapply(slot(x,"Polygons"), slot, "area")), sum)/1000000
 
 ##############################################################################
-###4) Make index raster grids of different cell size and rasterize
+###4) Make index raster grids of different cell size
 ##############################################################################
-r001 <- raster(ext=extent(dat), res=100, crs=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+#r001 <- raster(ext=extent(dat), res=100, crs=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 r1 <- raster(ext=extent(dat), res=1000, crs=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 r5 = aggregate(r1, 5)
 r10 = aggregate(r1, 10)
 r25 = aggregate(r1, 25)
 
+##############################################################################
+###5) VARIABLE1: Get fire count by cell
+##############################################################################
+
 ###Rasterize fire data - YOU ARE HERE!!!
-rdat10 <- rasterize(dat,r10, dat$MONTH, fun=mean)
-#rdat1 <- rasterize(dat,r1, dat$ID)
-rdat5 <- rasterize(dat,r5, "ID")
-plot(rdat10)
-names(rdat10)
-plot(raster(list[1]))
+#rdat1_1 <- rasterize(dat,r1, dat$ID, fun="count")
+#rdat5_1 <- rasterize(dat,r5, dat$ID, fun="count")
+#rdat10_1 <- rasterize(dat,r10, dat$ID, fun="count")
+rdat25_1 <- rasterize(dat,r25, dat$ID, fun="count")
+
+###Code to stack and extract to dataframe
+#x=stack(rdat10, rdat10, rdat10) 
+#rdat10_df=as.data.frame(x)
+
+##############################################################################
+###6) VARIABLE2: MEAN FIRE size (of intersected polygons)
+##############################################################################
+
+###Rasterize fire data - YOU ARE HERE!!!
+#rdat1_2 <- rasterize(dat,r1, dat$AREA_KM, fun="mean")
+#rdat5_2 <- rasterize(dat,r5, dat$AREA_KM, fun="mean")
+#rdat10_2 <- rasterize(dat,r10, dat$AREA_KM, fun="mean")
+rdat25_2 <- rasterize(dat,r25, dat$AREA_KM, fun="mean")
+
+##############################################################################
+###7) VARIABLE3: MEAN, MAX, MIN % area burnt per grid cell
+##############################################################################
+
+###Make raster grids into SpatialPolygons
+#r1sp=rasterToPolygons(r1)
+#r5sp=rasterToPolygons(r5)
+#r10sp=rasterToPolygons(r10)
+r25sp=rasterToPolygons(r25)
+
+###Fix dat to get around weird errors...
+gdat=gBuffer(dat,byid=TRUE,width=0)
+#?gIsValid #Use this function if you're getting errors...
+
+###Do intersection for each grid size
+#x=gIntersection(r1sp, gdat, byid = TRUE)
+#x$AREA_KM=sapply(sapply(slot(x, "polygons"), function(x) sapply(slot(x,"Polygons"), slot, "area")), sum)/1000000
+#y=as(x,"SpatialLinesDataFrame")
+#rdat1_3 = rasterize(y, r1, "AREA_KM", fun=mean)*100
+###ADD RASTERIZES FOR MAX MIN MEDIAN ETC...
+
+#x=gIntersection(r5sp, gdat, byid = TRUE)
+#x$AREA_KM=sapply(sapply(slot(x, "polygons"), function(x) sapply(slot(x,"Polygons"), slot, "area")), sum)/1000000
+#y=as(x,"SpatialLinesDataFrame")
+#rdat5_3 = (rasterize(y, r5, "AREA_KM", fun=mean)/25)*100
+
+#x=gIntersection(r10sp, gdat, byid = TRUE)
+#x$AREA_KM=sapply(sapply(slot(x, "polygons"), function(x) sapply(slot(x,"Polygons"), slot, "area")), sum)/1000000
+#y=as(x,"SpatialLinesDataFrame")
+#rdat10_3 = rasterize(y, r10, "AREA_KM", fun=mean) #in km, which happens to be % too
+
+x=gIntersection(r25sp, gdat, byid = TRUE)
+x$AREA_KM=sapply(sapply(slot(x, "polygons"), function(x) sapply(slot(x,"Polygons"), slot, "area")), sum)/1000000
+y=as(x,"SpatialLinesDataFrame")
+rdat25_3 = (rasterize(y, r25, "AREA_KM", fun=mean)/625)*100
 
 
+##############################################################################
+##############################################################################
+##############################################################################
 
-str(rdat10)
-rdat10@data
-rdat10 <- stack(rdat10, layers="MONTH")
-
-
-
-########Variabe 1####################################################
-     ########Count of fires per grid of different sizes#########
-###################################################################
-
-#### Create a raster with the number of fires per grid
-fireno25 <- rasterize(dat,r25, field="MONTH")
-fireno10 <- rasterize(dat,r10, field="ID", fun="count")
-fireno5 <- rasterize(dat,r5, field="ID", fun="count")
-#fireno1 <- rasterize(dat,r1, field="ID", fun="count")
-
-
-#### Work out a way to extract the info per grid
-
-########Variabe 2####################################################
-########MEAN FIRE size per grid of different sizes#########
-###################################################################
+###Useful functions
+?getValues
+?setValues
 
 # Load centroids derived from polygons 
 
@@ -173,35 +201,24 @@ proj4string(CNfireA)
 proj4string(BNPfireA)
 proj4string(GRNPfireA)
 
-
-
 ### make projections the same
-
-
 CPfireA=spTransform(CPfireA, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 BNPfireA=spTransform(BNPfireA, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 CNfireA=spTransform(CNfireA, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 GRNPfireA=spTransform(GRNPfireA, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
- 
- plot(CPfireA, col="blue")
- plot(BNPfireA, col="green", add=TRUE)
- plot(GRNPfireA, col="orange")
- plot(CNfireA, col="red")
+#plot
+plot(CNfireA, col="red")
+points(CPfireA, col="blue")
+points(BNPfireA, col="green")
+points(GRNPfireA, col="orange")
+
  
  
  CNfireA$ID=1:length(CNfireA)
  CNfireA$SOURCE=rep("CNfireA",length(CNfireA))
  CNfireA$areakm2=CNfireA$XAREA/1000000
- CNpercburn1=CNfireA$areakm2/1*100
- CNpercburn5=CNfireA$areakm2/5*100
- CNpercburn10=CNfireA$areakm2/10*100
- CNpercburn25=CNfireA$areakm2/25*100
- 
- 
- format(CNpercburn1,scientific=FALSE)
- 
- tapply(CNpercburn10, CN$MONTH, mean)
+
 
  GRNPfireA$YEAR=substr(GRNPfireA$DATE_BURNE, 1, 4)
  GRNPfireA$MONTH=as.numeric(substr(GRNPfireA$DATE_BURNE, 5, 6))
@@ -243,7 +260,7 @@ GRNPfireA=spTransform(GRNPfireA, CRSobj=CRS("+proj=utm +zone=34 +south +datum=WG
  data=spRbind(data, BNPfireA)
  data=spRbind(data, CPfireA)
  #dat <- spChFIDs(dat, as.character(1:length(dat)))
- data$YEAR_MONTH=paste(data$YEAR, data$MONTH, sep="/")
+ #data$YEAR_MONTH=paste(data$YEAR, data$MONTH, sep="/")
  #dat$DATE=
  
 ### Calculate percent area burns
